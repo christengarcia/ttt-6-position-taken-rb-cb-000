5ae533c06d5fa5fceec08caea38566deb1ce05aa
Christen Garcia <christen.garcia@gmail.com>
2017-10-25 05:07:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[0..8] == "" || board[0..8] == " " || board[0..8] == nil
-    false
-  elsif
-    true
-  end
+  board[0..8] == "" || board[0..8] == " " || board[0..8] == nil
+
 end
